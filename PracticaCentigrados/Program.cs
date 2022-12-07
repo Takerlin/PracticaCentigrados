fb2b8d6e4ba2e13be9c7d03d4e2cc0f2910dd96c
Takerlin Gereidi <takerlincalderon@outlook.com>
2022-12-06 18:13:29 -0800

    Agregar archivos de proyecto.

---
+++ PracticaCentigrados/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCentigrados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double c, f;
+
+            Console.WriteLine("Dame los grados centigrados");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            f = (c * 1.8) + 32;
+
+            Console.WriteLine("Los grados Farenheit son:{0}", f);
+
+        }
+    }
+}
